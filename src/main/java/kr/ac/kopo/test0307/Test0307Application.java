2be4e873427fd28ac0bd9adfe2f8066bb33b6f3a
starchaser777 <les980730@gmail.com>
2024-03-07 10:55:14 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/test0307/Test0307Application.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.test0307;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0307Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test0307Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kopo/test0307/Test0307ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.test0307;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0307ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
